019b1c2def90bb283f49104876e699d976d58752
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-16 13:15:30 +0900

    fix index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
 import App from './App';
-import * as reducers from './reducers';
+// import * as reducers from './reducers';
+import createStore from './createStore';
+// import { homedir } from 'os';
 
-const store = createStore(
-  combineReducers(reducers),
-  applyMiddleware(logger)
-);
+const store = createStore(history);
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 );
