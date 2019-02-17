7c5dfd2eac62c2b102474fb6131105fb8a25874c
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-17 23:55:56 +0900

    Add Ranking component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
+import Ranking from './components/Ranking';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          <li><Link to="/all">All Categories</Link></li>
+          <li><Link to="/category/2502">PCs, AVs</Link></li>
+          <li><Link to=""></Link></li>
+        </ul>
       </div>
     );
   }
---
+++ src/components/Ranking.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Ranking({ categoryId }){
+  return (
+    <div>
+      <h2>Ranking Component</h2>
+      <p>Category ID: {categoryId}</p>
+    </div>
+  )
+}
+Ranking.propTypes = {
+  categoryId: PropTypes.string
+};
+Ranking.defaultProps = {
+  categoryId: '1'
+};
