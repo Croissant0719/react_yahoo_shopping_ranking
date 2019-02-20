e254c01690ab6ca6f582d24ba2c6a7dd503620b3
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-20 23:53:14 +0900

    Add shopping and Ranking to reducers

---
+++ src/reducers/Ranking.js
@@ -0,0 +1,45 @@
+const getRanking = response => {
+  const ranking = [];
+  const itemLength = response.ResultSet.totalResultsReturned
+  for (let index = 0; index < itemLength; index++) {
+    const item = response.ResultSet['0'].Result[index + ''];
+    ranking.push({
+      code: item.Code,
+      name: item.Name,
+      url: item.Url,
+      imageUrl: item.Image.Medium,
+    })
+  }
+  return ranking;
+};
+
+const initialState = {
+  categoryId: undefined,
+  ranking: undefined,
+  error: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+    // Reset state on start request
+    case 'START_REQUEST':
+      return {
+        categoryId: action.payload.categoryId,
+        ranking: undefined,
+        error: false,
+      };
+
+    // Receive data
+    case 'RECEIVE_DATA':
+      return action.payload.error
+        ? { ...state, error: true}
+        : {
+          ...state,
+          ranking: getRanking(action.payload.response)
+        };
+
+      default:
+        return state;
+  }
+}
---
+++ src/reducers/shopping.js
@@ -0,0 +1,18 @@
+const initialState = {
+  categoried: [
+    {
+      id: '1',
+      name: 'All Categories',
+    },
+    {
+      id: '2502',
+      name: 'Computers and Peripherals'
+    },
+    {
+      id: '10002',
+      name: 'Books, Magazines, Comics',
+    }
+  ]
+};
+
+export default () => initialState;
