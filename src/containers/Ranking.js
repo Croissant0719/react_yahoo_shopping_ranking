76cbb25481777c527d73629858da026677daa962
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-20 23:28:42 +0900

    Add containers/Ranking

---
+++ src/containers/Ranking.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import Ranking from '../components/Ranking';
+import * as actions from '../actions/Ranking';
+
+const mapStateToProps = (state, ownProps) => ({
+  categoryId: ownProps.categoryId
+});
+
+const mapDispatchToProps = dispatch => ({
+  // connect onMount and onUpdate to fetchRanking
+  onMount (categoryId) {
+    dispatch(actions.fetchRanking(categoryId));
+  },
+  onUpdate (categoryId) {
+    dispatch(actions.fetchRanking(categoryId));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Ranking);
