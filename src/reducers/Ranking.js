c848a7f61541958e138a78c04fa1cf27493ab23e
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-23 23:34:28 +0900

    Change categoryId -> category

--- src/reducers/Ranking.js
+++ src/reducers/Ranking.js
@@ -14,7 +14,7 @@ const getRanking = response => {
 };
 
 const initialState = {
-  categoryId: undefined,
+  category: undefined,
   ranking: undefined,
   error: false
 };
@@ -24,7 +24,7 @@ export default (state = initialState, action) => {
     // Reset state on start request
     case "START_REQUEST":
       return {
-        categoryId: action.payload.categoryId,
+        category: action.payload.category,
         ranking: undefined,
         error: false
       };
